9fbed0aefd2193994e9d59ad6f7e25b5356c8020
Richard Tweed <RichardoC@users.noreply.github.com>
2023-03-20 21:51:31 +0000

    Add further testing

--- testing/generate-data.sh
+++ testing/generate-data.sh
@@ -46,21 +46,53 @@ kubectl run busybox-creation-kube-system --image=busybox --restart=Never --names
 kubectl -n kube-system delete pod busybox-creation-kube-system
 
 # - rule: Service Account Created in Kube Namespace
+# working
+kubectl -n kube-system create sa naughty-sa
 # - rule: System ClusterRole Modified/Deleted
+# working
+kubectl patch clusterrole.rbac.authorization.k8s.io/system:basic-user -p '{"metadata":{"labels":{"a":"b"}}}'
 # - rule: Attach to cluster-admin Role
+# working
+kubectl create clusterrolebinding --clusterrole=cluster-admin --serviceaccount default:abc abc-cluster-admin
 # - rule: ClusterRole With Wildcard Created
+# working
+kubectl create clusterrole --verb list --resource=* listy-mc-list-face
 # - rule: ClusterRole With Write Privileges Created
+# working
+kubectl create clusterrole --verb create --resource=pods writy-mc-list-facey
 # - rule: ClusterRole With Pod Exec Created
+# not working, but detected by Notice Created Role/ClusterRole with write privileges
+kubectl create role --verb create --resource pods/exec pod-exec-er
 # - rule: K8s Deployment Created
+# working
+kubectl create deployment test-depl --image busybox
 # - rule: K8s Deployment Deleted
+# working
+kubectl delete deployment test-depl 
 # - rule: K8s Service Created
+# working
+kubectl create svc clusterip test-svc  --tcp=8080:8080
 # - rule: K8s Service Deleted
+# working
+kubectl delete svc test-svc
 # # - rule: K8s ConfigMap Created
+# working
+kubectl create cm test-cm
 # # - rule: K8s ConfigMap Deleted
+# working
+kubectl delete cm test-cm
 # - rule: K8s Namespace Created
+# didn't work, matched Create Disallowed Namespace instead
+kubectl create ns test-ns
 # - rule: K8s Namespace Deleted
+# didn't work
+kubectl delete ns test-ns
 # - rule: K8s Serviceaccount Created
+# working
+kubectl create sa test-sa-d
 # - rule: K8s Serviceaccount Deleted
+# working
+kubectl delete sa test-sa-d
 # - rule: K8s Role/Clusterrole Created
 # - rule: K8s Role/Clusterrole Deleted
 # - rule: K8s Role/Clusterrolebinding Created
